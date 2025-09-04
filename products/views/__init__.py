3b2c4f437502473b91512deda1b8f39617e1600b
Pouya Javarsine <p.javarsine@yarhis.com>
2025-09-04 16:18:23 +0330

    delete useless apis

--- products/views/__init__.py
+++ products/views/__init__.py
@@ -1,6 +1,1 @@
-from .cart_add import cart_add
-from .cart_remove import cart_remove
-from .cart_detail import cart_detail
-from .checkout import checkout
-from .add_to_cart import add_to_cart
 from .Products import Products
